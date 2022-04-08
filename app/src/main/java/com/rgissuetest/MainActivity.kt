74db61ded1b6f27102ea539e53432265db04db7a
Kai Koenig <kai@ventego-creative.co.nz>
2022-04-08 14:29:07 +1200

    Mimicking client's RG Helper class and sending some simple errors this way.

--- app/src/main/java/com/rgissuetest/MainActivity.kt
+++ app/src/main/java/com/rgissuetest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rgissuetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.raygun.raygun4android.RaygunClient
 import com.rgissuetest.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,23 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+        initRaygun()
+
+        val tw = HashMap<String,String>()
+        tw["secondkey"] = "secondvalue"
+
+        // Manual exception creation & sending
+        RaygunClient.send(Exception("Congratulations, you have sent errors with Raygun4Android"), null, tw)
+
+        // A real exception will be thrown here, which will be caught & sent by RaygunClient
+        val i = 3 / 0
+        Log.d("Raygun4Android-Sample", "This is here purely so that our division by zero calculation in i gets used and not optimised away in a release build: $i")
+
+    }
+
+    private fun initRaygun() {
+        RaygunHelper.initRaygunClient(this)
+        // Set Raygun User Info
+        // RaygunHelper.setRaygunUserInfo(spDataManager, customerDetailsProvider)
     }
 }
---
+++ app/src/main/java/com/rgissuetest/RaygunHelper.kt
@@ -0,0 +1,36 @@
+package com.rgissuetest
+
+import android.app.Activity
+import android.util.Log
+import com.raygun.raygun4android.CrashReportingOnBeforeSend
+import com.raygun.raygun4android.RaygunClient
+import com.raygun.raygun4android.messages.crashreporting.RaygunMessage
+
+class RaygunHelper {
+    companion object {
+        fun initRaygunClient(mActivity: Activity) {
+            try {
+                RaygunClient.init(mActivity.application)
+                // Enable Real User Monitoring with Network Logging
+                // RaygunClient.enableRUM(mActivity, true)
+                // Enable Crash Reporting
+                RaygunClient.enableCrashReporting()
+                // Set App Version
+                RaygunClient.setVersion("1.0.0")
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            Log.d("RaygunHelper","### Init the Raygun client..")
+        }
+
+        class SampleOnBeforeSend : CrashReportingOnBeforeSend {
+            override fun onBeforeSend(message: RaygunMessage): RaygunMessage {
+                Log.d(
+                    "Raygun4Android-Sample",
+                    "In SampleOnBeforeSend - About to post to Raygun, returning the payload as is..."
+                )
+                return message
+            }
+        }
+    }
+}
